0f41918d4636161a1c8371c1e0e28e2d9a40eb69
547471846@qq.com <lyh1993@126.com>
2022-06-29 18:31:19 +0800

    Update Noxine.py

--- Noxine.py
+++ Noxine.py
@@ -4,6 +4,7 @@ from khl.card import CardMessage, Card, Module, Element, Types
 
 # 发起网络连接
 import requests as req
+import urllib.request as urq
 
 # 解析Html网页标签
 from bs4 import BeautifulSoup
@@ -25,17 +26,97 @@ import pytz
 import datetime
 
 # ----------replit----------
-# import os
-# from keep_alive import keep_alive
-# token = os.environ['token']
+import os
+from keep_alive import keep_alive
+
+token = os.environ['token']
 # ----------replit----------
 # ----------local----------
-with open('config/config.json', 'r', encoding='utf-8') as f:
-    config = json.load(f)
-token = config['token']
+# with open('config/config.json', 'r', encoding='utf-8') as f:
+#     config = json.load(f)
+# token=config['token']
 # ----------local----------
 bot = Bot(token)
 # 初始化机器人
+
+
+def stdcard(mode: str, title: str, link: str, img: str, date: str):
+    if mode == 'wx':
+        btn = '搜索文章'
+        avatar = 'http://wx.qlogo.cn/mmhead/Q3auHgzwzM7ZnPbIDnIibUDKyzYRAagHD1Q3lWfeq7TZRnyW4ABYiacg/0'
+        author = 'Wakfu真好玩'
+        icon = 'https://weixin.qq.com/zh_CN/htmledition/images/wechat_logo_109.2x219536.png'
+        media = '微信公众平台'
+    if mode == 'blzl':
+        btn = '阅读专栏'
+        avatar = 'https://i0.hdslb.com/bfs/face/a223372898c0e37fcb6e632a5ba444c34b9ef6ec.jpg'
+        author = '盐十汽水'
+        icon = 'https://img.kaiheila.cn/assets/2022-06/T5M0eIAcme06402x.png'
+        media = '哔哩哔哩弹幕网'
+    cm = CardMessage(
+        Card(
+            Module.Section(
+                Element.Text(f'**{title}**', Types.Text.KMD),
+                Element.Button(btn,
+                               link,
+                               Types.Click.LINK,
+                               theme=Types.Theme.SUCCESS)),
+            Module.Context(Element.Image(avatar), Element.Text(author)),
+            Module.Container(Element.Image(img)),
+            Module.Context(Element.Image(icon),
+                           Element.Text(f'{media} · {date}'))))
+    return cm
+# 函数1【stdcard】标准卡片格式一：最新资讯
+
+
+def wxgethtml(type: int, title: str = '', onlyurl='false'):
+    global wxtitle, wxdate
+    url = f'http://weixin.sogou.com/weixin?type={type}&s_from=input&query=Wakfu%E7%9C%9F%E5%A5%BD%E7%8E%A9+{title}&ie=utf8&_sug_=n'
+    if onlyurl == 'false':
+        header = {
+            'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
+        }
+        html = req.get(url, headers=header)
+        soup = BeautifulSoup(html.text, 'html.parser')
+        if type == 1:
+            articletag = soup.find_all('a',
+                                       attrs={'uigs': 'account_article_0'})[0]
+            wxtitle = articletag.string
+            date = int(
+                re.findall(r"\d+",
+                           str(articletag.next_element.next_element))[0])
+            timezone = pytz.timezone('Asia/Shanghai')
+            localtime = datetime.datetime.fromtimestamp(date, timezone)
+            wxdate = localtime.strftime("%Y-%m-%d %H:%M")
+            return {"date": wxdate, "title": wxtitle}
+        if type == 2:
+            articletag = soup.find('img',
+                                   attrs={'onerror':
+                                          'errorImage(this)'})['src']
+            return f'https:{articletag}'
+    else:
+        return url
+# 函数2【wxgethtml】搜狗微信获取链接
+
+
+cyresp = urq.urlopen('https://launcher.cdn.ankama.com/cytrus.json')
+cyjson = json.loads(cyresp.read())
+with open('cytrus.txt', 'r') as f:
+    cylocal = json.load(f)
+# 读取Ankama游戏最新版本页，读入本地缓存页备用
+
+
+def check(game):
+    global version
+    version = cyjson['games'][str(game)]['platforms']['windows']['main']
+    temp = cylocal['games'][str(game)]['platforms']['windows']
+    if version != temp['main']:
+        temp['main'] = version
+        with open('cytrus.txt', 'w') as f:
+            json.dump(cylocal, f)
+        return 'True'
+# 函数3【check】检测Ankama游戏版本更新
 
 
 @bot.command(name='roll')
@@ -47,33 +128,46 @@ async def roll(msg: Message, t_min: int = 1, t_max: int = 100):
 
 @bot.command(name='bd')
 async def bd(msg: Message, str: str):
-    cm = CardMessage(Card(
-        Module.Section(
-            Element.Text(f'需要我帮您百度一下【{str}】吗？', Types.Text.PLAIN),
-            Element.Button(
-                '百度一下', f'https://www.baidu.com/s?wd={str}', Types.Click.LINK, theme=Types.Theme.INFO)
-        )
-    ))
+    cm = CardMessage(
+        Card(
+            Module.Section(
+                Element.Text(f'需要我帮您百度一下【{str}】吗？', Types.Text.PLAIN),
+                Element.Button('百度一下',
+                               f'https://www.baidu.com/s?wd={str}',
+                               Types.Click.LINK,
+                               theme=Types.Theme.INFO))))
     await msg.ctx.channel.send(cm)
 # 指令2【/bd string】百度搜索
 
 
 @bot.command(name='bl')
 async def bl(msg: Message, str: str):
-    cm = CardMessage(Card(
-        Module.Section(
-            Element.Text(f'需要我帮您哔哩哔哩一下【{str}】吗？', Types.Text.PLAIN),
-            Element.Button(
-                '哔哩哔哩', f'https://search.bilibili.com/all?keyword={str}', Types.Click.LINK, theme=Types.Theme.DANGER)
-        )
-    ))
+    cm = CardMessage(
+        Card(
+            Module.Section(
+                Element.Text(f'需要我帮您哔哩哔哩一下【{str}】吗？', Types.Text.PLAIN),
+                Element.Button(
+                    '哔哩哔哩',
+                    f'https://search.bilibili.com/all?keyword={str}',
+                    Types.Click.LINK,
+                    theme=Types.Theme.DANGER))))
     await msg.ctx.channel.send(cm)
 # 指令3【/bl string】b站搜索
 
 
 @bot.command(name='rep')
-async def rep(msg: Message, str: str):    
-    await msg.ctx.channel.send(str)
+async def rep(msg: Message,
+              str: str,
+              card='false',
+              link='',
+              img='',
+              date='',
+              mode='wx'):
+    if card == 'false':
+        await msg.ctx.channel.send(str)
+    if card == 'true':
+        cm = stdcard(mode, str, 'https://' + link, 'https://' + img, date)
+        await msg.ctx.channel.send(cm)
 # 指令4【/rep string】复读机
 
 
@@ -131,75 +225,33 @@ async def checkwx():
         with open('wx.txt', 'w') as write_f:
             write_f.write(json.dumps(wxjson))
         wximg = wxgethtml(2, wxtitle)
-        cm = stdcard('wx', wxtitle, wxgethtml(
-            2, wxtitle, 'true'), wximg, wxdate)
+        cm = stdcard('wx', wxtitle, wxgethtml(2, wxtitle, 'true'), wximg,
+                     wxdate)
         channel = await bot.fetch_public_channel('3446958221309451')
         await bot.send(channel, cm)
 # 定时检测1【checkwx】微信最新文章
 
 
-def stdcard(mode: str, title: str, link: str, img: str, date: str):
-    if mode == 'wx':
-        btn = '搜索文章'
-        avatar = 'http://wx.qlogo.cn/mmhead/Q3auHgzwzM7ZnPbIDnIibUDKyzYRAagHD1Q3lWfeq7TZRnyW4ABYiacg/0'
-        author = 'Wakfu真好玩'
-        icon = 'https://weixin.qq.com/zh_CN/htmledition/images/wechat_logo_109.2x219536.png'
-        media = '微信公众平台'
-    cm = CardMessage(Card(
-        Module.Section(
-            Element.Text(f'**{title}**', Types.Text.KMD),
-            Element.Button(btn, link, Types.Click.LINK,
-                           theme=Types.Theme.SUCCESS)
-        ),
-        Module.Context(
-            Element.Image(avatar),
-            Element.Text(author)
-        ),
-        Module.Container(
-            Element.Image(img)
-        ),
-        Module.Context(
-            Element.Image(icon),
-            Element.Text(f'{media} · {date}')
-        )
-    ))
-    return cm
-# 函数1【stdcard】标准卡片格式一：最新资讯
-
-
-def wxgethtml(type: int, title: str = '', onlyurl='false'):
-    global wxtitle, wxdate
-    url = f'http://weixin.sogou.com/weixin?type={type}&s_from=input&query=Wakfu%E7%9C%9F%E5%A5%BD%E7%8E%A9+{title}&ie=utf8&_sug_=n'
-    if onlyurl == 'false':
-        header = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
-        html = req.get(url, headers=header)
-        soup = BeautifulSoup(html.text, 'html.parser')
-        if type == 1:
-            articletag = soup.find_all(
-                'a', attrs={'uigs': 'account_article_0'})[0]
-            wxtitle = articletag.string
-            date = int(re.findall(
-                r"\d+", str(articletag.next_element.next_element))[0])
-            timezone = pytz.timezone('Asia/Shanghai')
-            localtime = datetime.datetime.fromtimestamp(date, timezone)
-            wxdate = localtime.strftime("%Y-%m-%d %H:%M")
-            return {"date": wxdate, "title": wxtitle}
-        if type == 2:
-            articletag = soup.find(
-                'img', attrs={'onerror': 'errorImage(this)'})['src']
-            return f'https:{articletag}'
+@bot.task.add_interval(minutes=5)
+async def checkgame():
+    gamelist = ['dofus', 'krosmaga', 'wakfu', 'waven']
+    if cyjson == cylocal:
+        print('same')
     else:
-        return url
-# 函数2【wxgethtml】搜狗微信获取链接
-
+        for item in gamelist:
+            if check(item):
+                cm = f'【{item.title()}】更新{version[4:]}版本啦~'
+                break
+        channel = await bot.fetch_public_channel('8601971711684857')
+        await bot.send(channel, cm)
+# 定时检测2【checkgame】Ankama游戏版本更新
 
 # 系统日志，随便写一个，那就都写吧
 logging.getLogger().setLevel(logging.INFO)
 logging.basicConfig(level='INFO')
 
 # ----------replit----------
-# keep_alive()
+keep_alive()
 # ----------replit----------
 
 # 万事俱备，机械虫出发！
